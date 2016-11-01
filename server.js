6e36ee9a2e1437e42c396ec09fc40add49b92f5a
David Chesnut <davech@microsoft.com>
2016-11-01 12:18:57 -0700

    added copyright info

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+// See LICENSE in the project root for license information.
+
 var express = require('express');
 var serveStatic = require('serve-static');
 var argv = require('optimist')
